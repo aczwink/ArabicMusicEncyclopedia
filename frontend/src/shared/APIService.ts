da93b054cadfd543d875f714e73c43e3bc369a04
Amir Czwink <amir130@hotmail.de>
2022-06-19 19:55:54 +0200

    api service generalization

--- frontend/src/shared/APIService.ts
+++ frontend/src/shared/APIService.ts
@@ -15,87 +15,20 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * */
-import { Dictionary, URL } from "acts-util-core";
-import { HTTPMethod, HTTPService, Injectable, RequestHeaders } from "acfrontend";
+import { APIServiceBase, HTTPService, Injectable } from "acfrontend";
 import { g_backendAuthority, g_backendProtocol } from "../backend";
-import { API, RequestData, ResponseData } from "../../dist/api";
+import { API } from "../../dist/api";
 
 @Injectable
 export class APIService extends API
 {
-    constructor(private httpService: HTTPService)
+    constructor(httpService: HTTPService)
     {
-        super();
+        super( req => this.base.IssueRequest(req) );
+
+        this.base = new APIServiceBase(httpService, g_backendAuthority, g_backendProtocol);
     }
 
-    //Protected methods
-    protected async IssueRequest<SuccessStatusCodeType, ErrorStatusCodeType, DataType>(requestData: RequestData): Promise<ResponseData<SuccessStatusCodeType, ErrorStatusCodeType, DataType>>
-    {
-        const url = new URL({
-            authority: g_backendAuthority,
-            path: requestData.path,
-            protocol: g_backendProtocol,
-            queryParams: (requestData.query === undefined) ? {} : (requestData.query as Dictionary<string>)
-        });
-        const response = await this.httpService.SendRequest({
-            body: this.FormatBody(requestData.body, requestData.requestBodyType),
-            headers: this.CreateHeaders(requestData.body, requestData.requestBodyType),
-            method: requestData.method,
-            responseType: requestData.responseType,
-            url: url.ToString()
-        });
-
-        if(response.statusCode === requestData.successStatusCode)
-        {
-            return {
-                statusCode: response.statusCode as unknown as SuccessStatusCodeType,
-                data: response.body
-            };
-        }
-        return {
-            statusCode: response.statusCode as unknown as ErrorStatusCodeType
-        };
-    }
-
-    //Private methods
-    private CreateHeaders(body: object | undefined, requestBodyType: "form-data" | undefined): RequestHeaders
-    {
-        if((body !== undefined) && (requestBodyType !== "form-data"))
-        {
-            return {
-                "Content-Type": "application/json"
-            };
-        }
-        return {};
-    }
-
-    private FormatBody(body: object | undefined, requestBodyType: "form-data" | undefined): any
-    {
-        if(body === undefined)
-            return undefined;
-
-        if(requestBodyType === "form-data")
-        {
-            //let XmlHttpRequest do this since it also sets the boundary
-            //headers["Content-Type"] = "multipart/form-data";
-            const fd = new FormData();
-
-            for (const key in body)
-            {
-                if (Object.prototype.hasOwnProperty.call(body, key))
-                {
-                    fd.append(key, (body as any)[key]);
-                }
-            }
-
-            return fd;
-        }
-
-        return JSON.stringify(body);
-    }
-    //TODO: remove the following legacy methods
-    public Request<T>(route: string, method: HTTPMethod, data: any, routeParams?: any): Promise<T>
-    {
-        throw new Error("Legacy method 'APIService.Request' called");
-    }
+    //Private variables
+    private base: APIServiceBase;
 }
