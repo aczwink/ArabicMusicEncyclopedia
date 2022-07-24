173623df4f77cb77783e7b71948a4d786bc4a8ec
Amir Czwink <amir130@hotmail.de>
2022-07-25 01:14:14 +0200

    missing files for deployment

--- apache/deploy.sh
+++ apache/deploy.sh
@@ -6,10 +6,12 @@ rm -rf $deployPath
 mkdir $deployPath
 
 cp $frontendPath/index.htm $deployPath/
+cp $frontendPath/emmentaler-26.woff $deployPath/
 cp $distPath/acts-util-core.js $deployPath/
 cp $distPath/acfrontend.js $deployPath/
 cp $distPath/bundle.js $deployPath/
 cp $distPath/clean.css $deployPath/
+cp $distPath/site.css $deployPath/
 cp -r $frontendPath/images $deployPath/
 
 chown -R www-data:www-data $deployPath
